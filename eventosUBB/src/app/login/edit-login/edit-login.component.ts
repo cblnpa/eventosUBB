458b99454b495171b8a1f51ac63fe40aa7e67bdd
Gastón <gaston.lara.p@gmail.com>
2019-07-01 14:31:04 -0400

    actualizacion

--- eventosUBB/src/app/login/edit-login/edit-login.component.ts
+++ eventosUBB/src/app/login/edit-login/edit-login.component.ts
@@ -2,9 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { filter, map } from 'rxjs/operators';
 import { Router, ActivationEnd } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { AngularFileUploaderModule } from "angular-file-uploader";
 
 import { UserService } from '../../servicios/servicio.index';
 import { users } from '../../model/users';
+import {global} from '../../servicios/global'
 
 declare function init_plugins();
 @Component({
@@ -22,6 +24,30 @@ export class EditLoginComponent implements OnInit {
   public status: string;
   public identity;
   public token;
+  public afuConfig = {
+    multiple: false,
+    formatsAllowed: ".jpg,.png, .jpeg, .gif",
+    maxSize: "50",
+    uploadAPI:  {
+      url:global.url+'user/upload',
+      headers: {
+     "Authorization" : this.userService.getToken()
+      }
+    },
+    theme: "attachPin",
+    hideProgressBar: false,
+    hideResetBtn: true,
+    hideSelectBtn: false,
+    replaceTexts: {
+      selectFileBtn: 'Select Files',
+      resetBtn: 'Reset',
+      uploadBtn: 'Upload',
+      dragNDropBox: 'Drag N Drop',
+      attachPinBtn: 'Attach Files...',
+      afterUploadMsg_success: 'Successfully Uploaded !',
+      afterUploadMsg_error: 'Upload Failed !'
+    }
+};
   constructor( private userService: UserService, private router: Router, private title: Title ) { 
     this.user = new users('','','','',''); 
     this.identity = this.userService.getIdentity();
@@ -58,7 +84,30 @@ export class EditLoginComponent implements OnInit {
   onSubmit(form){
     this.userService.update(this.token, this.user).subscribe(
       response => {
-        console.log(response);
+        if(response && response.status){
+          this.status = 'success';
+
+          //Actualizar usuario de sesion
+          if(response.changes.nombreUsuario){
+            this.user.nombreUsuario = response.changes.nombreUsuario;
+          }
+          if(response.changes.apellidoUsuario){
+            this.user.apellidoUsuario = response.changes.apellidoUsuario;
+          }
+          if(response.changes.email){
+            this.user.email = response.changes.email;
+          }
+          if(response.changes.password){
+            this.user.password = response.changes.password;
+          }
+          if(response.changes.avatar){
+            this.user.avatar = response.changes.avatar;
+          }
+
+          this.identity = this.user;
+          localStorage.setItem('identity', JSON.stringify(this.identity));
+        }
+
       },
       error =>{
         this.status='error';
