87468773a3a6a0c5e8532243f1ea37a1c0557517
Camila <camilapinto@outlook.cl>
2019-08-09 22:49:10 -0400

    Crear Unidades *working

--- eventosUBB/src/app/componentes/unidades/unidades-crear/unidades-crear.component.ts
+++ eventosUBB/src/app/componentes/unidades/unidades-crear/unidades-crear.component.ts
@@ -1,91 +1,154 @@
 import { Component, OnInit } from '@angular/core';
-import { UnidadService, UserService } from '../../../servicios/servicio.index';
-import { global } from '../../../servicios/global';
 import { Router, ActivationEnd } from '@angular/router';
+
+import { UnidadService, UserService, CiudadService } from '../../../servicios/servicio.index';
+import { global } from '../../../servicios/global';
 import { Title } from '@angular/platform-browser';
-import { unidad } from '../../../model/model.index';
+import { unidad, users, ciudad } from '../../../model/model.index';
 import { filter, map } from 'rxjs/operators';
 
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-unidades-crear',
   templateUrl: './unidades-crear.component.html',
   styleUrls: ['./unidades-crear.component.css'],
-  providers: [ UnidadService, UserService ]
+  providers: [UnidadService, UserService]
 })
 export class UnidadesCrearComponent implements OnInit {
 
-  titulo: string; 
+  public titulo: string;
 
   public url; //url del backend
   public unidad: unidad; //objeto de tipo unidad 
   public token;
+  public identity;
+
+  //variables para el select
+  public usuario: users;
+  public usuarios: any = []; //almacenar los usuarios
+  public optionsUsuario;
+
+  public ciudad: ciudad;
+  public ciudades: any = []; //almacena las ciudades (o sedes)
+  public optionsCiudad;
+
+  //Configuraciones del ngx-select-dropdown
+  configUsuario = {
+    displayKey: 'nombreUsuario', //if objects array passed which key to be displayed defaults to description
+    search: true, //true/false for the search functionlity defaults to false,
+    height: '150px', //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
+    placeholder: 'Seleccionar usuario', // text to be displayed when no item is selected defaults to Select,
+    noResultsFound: '¡No se encuentra el usuario!', // text to be displayed when no items are found while searching
+    searchPlaceholder: 'Buscar usuario', // label thats displayed in search input,
+    searchOnKey: 'nombreUsuario' // key on which search should be performed this will be selective search. if undefined this will be extensive search on all keys
+  }
+
+  configCiudad = {
+    displayKey: 'nombreCiudad', //if objects array passed which key to be displayed defaults to description
+    search: true, //true/false for the search functionlity defaults to false,
+    height: '150px', //height of the list so that if there are more no of items it can show a scroll defaults to auto. With auto height scroll will never appear
+    placeholder: 'Seleccionar sede', // text to be displayed when no item is selected defaults to Select,
+    noResultsFound: '¡No se encuentra la sede!', // text to be displayed when no items are found while searching
+    searchPlaceholder: 'Buscar sede', // label thats displayed in search input,
+    searchOnKey: 'nombreCiudad' // key on which search should be performed this will be selective search. if undefined this will be extensive search on all keys
+  }
 
   //Subir imagen
   public afuConfig = {
     multiple: false,
     formatsAllowed: ".jpg,.jpeg,.png,.gif",
     maxSize: "50",
-    uploadAPI:  {
-      url:global.url+'logo',
+    uploadAPI: {
+      url: global.url + 'logo',
       headers: {
-        "Authorization" : this.userService.getToken()
-         }
+        "Authorization": this.userService.getToken()
+      }
     },
-     theme: "attachPin",
-     hideProgressBar: false,
-     hideResetBtn: true,
-     hideSelectBtn: false,
-     attachPinText: 'Subir logo',
-     replaceTexts: {
-       attachPinBtn: 'Seleccionar archivo',
-       afterUploadMsg_success: 'Archivo seleccionado exitosamente'
-     }
-};
+    theme: "attachPin",
+    hideProgressBar: false,
+    hideResetBtn: true,
+    hideSelectBtn: false,
+    attachPinText: 'Subir logo',
+    replaceTexts: {
+      attachPinBtn: 'Seleccionar archivo',
+      afterUploadMsg_success: 'Archivo seleccionado exitosamente'
+    }
+  };
 
-  constructor( private router: Router, private title: Title, private unidadService: UnidadService, 
-    private userService: UserService ) {
-    this.unidad = new unidad('','','','');
+  constructor(private router: Router, private title: Title, private unidadService: UnidadService,
+    private userService: UserService, private ciudadService: CiudadService) {
+    this.unidad = new unidad('', '', '', '');
+    this.usuario = new users('', '', '', '', '', null, null, null);
+    this.ciudad = new ciudad(null, '');
     this.url = global.url;
     this.token = this.userService.getToken();
-
+    this.identity = this.userService.getIdentity();
 
     //Para mostrar el título de la página actual
     this.getDataRoute()
-    .subscribe( data => {
-      console.log(data);
-      this.titulo = data.titulo;
-      this.title.setTitle('EventosUBB - ' + this.titulo);
-    });
-   }
+      .subscribe(data => {
+        console.log(data);
+        this.titulo = data.titulo;
+        this.title.setTitle('EventosUBB - ' + this.titulo);
+      });
+  }
 
   ngOnInit() {
+    this.getUsuarios();
+    this.getCiudades();
   }
 
   //Función para obtener el nombre de la página actual
-  getDataRoute(){
+  getDataRoute() {
     return this.router.events.pipe(
-      filter(evento => evento instanceof ActivationEnd ),
-      filter( (evento:ActivationEnd) => evento.snapshot.firstChild === null ),
-      map( (evento:ActivationEnd) => evento.snapshot.data )
+      filter(evento => evento instanceof ActivationEnd),
+      filter((evento: ActivationEnd) => evento.snapshot.firstChild === null),
+      map((evento: ActivationEnd) => evento.snapshot.data)
     )
   }
 
   //
-  guardarUnidad(form){
+  guardarUnidad(form) {
+    this.unidad.email = this.usuarios.email;
+    this.unidad.sede = this.ciudades.nombreCiudad;
     this.unidadService.guardarUnidad(this.unidad).subscribe(
       response => {
-        console.log(response);
+        if(response.status == 'success'){
+          Swal.fire({
+            type: 'success',
+            title: '¡Se ha creado con éxito la unidad!'
+          });
+        }
+        this.router.navigate(['/verUnidades']);
       },
       error => {
         console.log(<any>error);
-      }
-    )
+      })
+  }
 
+  getUsuarios() {
+    this.userService.getAll().subscribe(
+      response => {
+        this.optionsUsuario = response.users;
+      },
+      error => {
+        console.log(<any>error);
+      })
+  }
+
+  getCiudades() {
+    this.ciudadService.getCiudades().subscribe(
+      response => {
+        this.optionsCiudad = response.ciudad;
+      },
+      error => {
+        console.log(<any>error);
+      })
   }
 
   //Función para subir el logo
-  logoUpload(logo){
+  logoUpload(logo) {
     let data = JSON.parse(logo.response);
     console.log(logo.response);
     this.unidad.logoUnidad = data.image;
