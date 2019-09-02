c9dcb68857bcb8aa538dbaac5e6d7ca6404cccd7
Camila Pinto <camilapinto@outlook.cl>
2019-09-02 14:20:00 -0400

    Paginación en comision-ver

--- eventosUBB/src/app/componentes/comisiones/comision-ver/comision-ver.component.ts
+++ eventosUBB/src/app/componentes/comisiones/comision-ver/comision-ver.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort, MatSortable, MatTableDataSource } from '@angular/material';
+import { MatSort, MatPaginator, MatTableDataSource, MatPaginatorIntl } from '@angular/material';
 import { EventoUsersService, UserService } from '../../../servicios/servicio.index';
 import { global } from '../../../servicios/global'
 import { Router } from '@angular/router';
@@ -14,34 +14,46 @@ export class ComisionVerComponent implements OnInit {
   public url;
   public eventos;
   public identity;
+  public cantEventos; //número de eventos encontrados
 
   //Data sorting
   displayedColumns: string[] = ['idEvento', 'nombreEvento', 'button'];
   public dataSource;
 
   constructor(private eventoUsersService: EventoUsersService, private userService: UserService,
-    private router: Router) {
+    private router: Router, public paginatorSettings: MatPaginatorIntl) {
     this.url = global.url;
     this.identity = this.userService.getIdentity();
   }
 
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatSort) sort: MatSort; //ordenar datos de la tabla
+  @ViewChild(MatPaginator) paginator: MatPaginator; //paginación de la tabla
 
   ngOnInit() {
     // this.getMisEventosAdmin();
     this.eventoUsersService.getMisEventosAdmin(this.identity.sub).subscribe(
       response => {
+        console.log(response);
+        this.cantEventos = response.eventos.length;
         this.dataSource = new MatTableDataSource(response.eventos);
         this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
       },
       error => {
         console.log(<any>error);
-      })
+      });
+    this.paginadorSettings();
   }
 
   //Redirección a la vista administrativa del evento
   eventosDetalles(idEvento: number) {
     this.router.navigate(['/eventoDetalle/' + idEvento]);
+  }
+
+  paginadorSettings() {
+    this.paginatorSettings.itemsPerPageLabel = 'Elementos por página';
+    this.paginatorSettings.previousPageLabel = 'Página anterior';
+    this.paginatorSettings.nextPageLabel = 'Página siguiente';
   }
 
 }
