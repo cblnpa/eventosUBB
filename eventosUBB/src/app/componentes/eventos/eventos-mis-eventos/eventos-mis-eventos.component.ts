f6f428827d94e0f582557f6ada8f4630a5d20247
Camila Pinto <camilapinto@outlook.cl>
2019-09-03 19:51:47 -0400

    eventos-mis eventos *almost

--- eventosUBB/src/app/componentes/eventos/eventos-mis-eventos/eventos-mis-eventos.component.ts
+++ eventosUBB/src/app/componentes/eventos/eventos-mis-eventos/eventos-mis-eventos.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatPaginator, MatTableDataSource, MatPaginatorIntl } from '@angular/material';
+import { Router } from '@angular/router';
 import { global } from '../../../servicios/global';
 
 import { EventoUsersService, UserService, EventoPojoService } from '../../../servicios/servicio.index';
@@ -8,7 +9,7 @@ import { EventoUsersService, UserService, EventoPojoService } from '../../../ser
   selector: 'app-eventos-mis-eventos',
   templateUrl: './eventos-mis-eventos.component.html',
   styleUrls: ['./eventos-mis-eventos.component.css'],
-  providers: [ EventoUsersService, UserService, EventoPojoService ]
+  providers: [EventoUsersService, UserService, EventoPojoService]
 })
 export class EventosMisEventosComponent implements OnInit {
 
@@ -25,28 +26,35 @@ export class EventosMisEventosComponent implements OnInit {
   public perfil; // id del perfil del usuario activo
   public sub; // pruebas para el login con google el sub es el id del usuario
 
-  constructor( private eventoUsersService: EventoUsersService,  private userService: UserService, 
-    private router: Router ) {
+  //Data sorting
+  displayedColumns: string[] = ['nombreEvento', 'created_at', 'button'];
+  dataSource;
+  cantidadEventos: number;
+  filtrar: string;
 
+  constructor(private eventoUsersService: EventoUsersService, private userService: UserService,
+    private router: Router, public paginatorSettings: MatPaginatorIntl) {
     this.url = global.url;
     this.token = this.userService.getToken();
     this.identity = this.userService.getIdentity();
-
   }
+
+  @ViewChild(MatSort) sort: MatSort; //ordenar datos de la tabla
+  @ViewChild(MatPaginator) paginator: MatPaginator; //paginación de la tabla
 
   ngOnInit() {
     this.perfil = this.identity.perfil_idPerfil;
     this.sub = this.identity.sub;
-  
+    this.paginadorSettings();
     this.getMisEventos();
     this.getMisEventosAdmin();
     this.getUsuarios();
   }
 
   //Obtener los eventos en los que el usuario participa 
-  getMisEventos(){
+  getMisEventos() {
     this.eventoUsersService.getMisEventos(this.sub).subscribe(
-      response => {  
+      response => {
         this.misEventos = response.eventos;
       },
       error => {
@@ -56,13 +64,17 @@ export class EventosMisEventosComponent implements OnInit {
   }
 
   //Obtener los eventos que el usuario administra
-  getMisEventosAdmin(){
+  getMisEventosAdmin() {
     console.log(this.sub);
     this.eventoUsersService.getMisEventosAdmin2(this.sub).subscribe(
       response => {
         console.log(response);
         this.misEventosAdmin = response.eventos;
         console.log(this.misEventosAdmin);
+        this.cantidadEventos = response.eventos.length; //cantidad de eventos
+        this.dataSource = new MatTableDataSource(response.eventos);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
       },
       error => {
         console.log(<any>error);
@@ -70,7 +82,7 @@ export class EventosMisEventosComponent implements OnInit {
     )
   }
 
-  getUsuarios(){
+  getUsuarios() {
     this.eventoUsersService.getEventoUsersById(this.idEvento).subscribe(
       response => {
         console.log(response);
@@ -78,13 +90,19 @@ export class EventosMisEventosComponent implements OnInit {
     )
   }
 
+  paginadorSettings() {
+    this.paginatorSettings.itemsPerPageLabel = 'Elementos por página';
+    this.paginatorSettings.previousPageLabel = 'Página anterior';
+    this.paginatorSettings.nextPageLabel = 'Página siguiente';
+  }
+
   //Redirección a la vista administrativa del evento
-  eventosDetalles(idEvento: number){
+  eventosDetalles(idEvento: number) {
     this.router.navigate(['/eventoDetalle/' + idEvento]);
   }
 
   //Redirección a la vista general del evento
-  verEvento(idEvento: number){
+  verEvento(idEvento: number) {
     this.router.navigate(['/eventoDetallePublic/' + idEvento]);
 
   }
