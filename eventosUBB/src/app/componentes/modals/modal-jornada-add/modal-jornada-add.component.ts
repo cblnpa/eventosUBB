9bdfc86893757bbd95f9a03c1fcc83ff7e7c9ae5
Camila Pinto <camilapinto@outlook.cl>
2019-09-05 15:37:55 -0400

    cambio de base de datos + modal jornada

--- eventosUBB/src/app/componentes/modals/modal-jornada-add/modal-jornada-add.component.ts
+++ eventosUBB/src/app/componentes/modals/modal-jornada-add/modal-jornada-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ModalService, JornadaService } from '../../../servicios/servicio.index';
 import { jornada } from '../../../model/jornada';
 import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-modal-jornada-add',
@@ -20,18 +21,32 @@ export class ModalJornadaAddComponent implements OnInit {
   ngOnInit() {
   }
 
-  ocultarModal() {
+  //pregunta si quiere salir del modal
+  salirModal() {
+    this.modalService.salirModal();
+  }
+
+  //oculta el modal luego de agregar los datos
+  ocultarModal(){
     this.modalService.ocultarModal();
   }
 
   //Formulario para agregar jornada
   agregarJornada(form) {
+    console.log('antes del servicio');
     this.route.params.subscribe(params => {
       let id = +params['id'];
       this.jornadaAdd.evento_idEvento = id;
+      console.log(id);
+      console.log(this.jornadaAdd);
       this.jornadaService.guardarJornada(this.jornadaAdd).subscribe(
         response => {
           console.log(response);
+          Swal.fire({
+            type: 'success',
+            title: 'Creado con éxito',
+            text: 'Se ha creado la jornada sin ningún problema',
+          })
         },
         error => {
           console.log(<any>error);
--- eventosUBB/src/app/servicios/global.ts
+++ eventosUBB/src/app/servicios/global.ts
@@ -1,3 +1,5 @@
 export var global = {
-    url: 'http://localhost:8000/api/'
+    // url: 'http://localhost:8000/api/'
+    //url: 'http://parra.chillan.ubiobio.cl:8090/~gaston.lara1401/eventosUBB-laravel/api/'
+    url: 'http://parra.chillan.ubiobio.cl:8090/~gaston.lara1401/eventosUBB-laravel/public/api/'
 }
