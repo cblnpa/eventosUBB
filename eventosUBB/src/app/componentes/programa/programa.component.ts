9b38ed3d770195f98ab49d0a808d6290b448df1b
Camila Pinto <camilapinto@outlook.cl>
2019-11-11 14:27:29 -0300

    pdf funcionando (programa)

--- eventosUBB/src/app/componentes/programa/programa.component.ts
+++ eventosUBB/src/app/componentes/programa/programa.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EventoPojoService } from '../../servicios/servicio.index';
-import * as jsPDF from 'jspdf';
+// import * as jsPDF from 'jspdf';
+
+declare const require: any;
+const jsPDF = require('jspdf');
+require ('jspdf-autotable');
 
 @Component({
   selector: 'app-programa',
@@ -91,27 +95,71 @@ export class ProgramaComponent implements OnInit {
   }
 
   @ViewChild('content') content: ElementRef;
-  
+
+  // downloadPDF() {
+
+  //   //   const elementToPrint = document.getElementById('pdf'); //The html element to become a pdf
+  //   //   const pdf = new jsPDF('auto', 'px', 'a4');
+  //   //   pdf.addHTML(elementToPrint, () => {
+  //   //     pdf.save('web.pdf');
+  //   // });
+
+  //   let doc = new jsPDF({orientation: 'p', format: 'letter', putOnlyUsedFonts: true});
+  //   // let ElementHandlers = {
+  //   //   '#editor': function (element, renderer) {
+  //   //     return true;
+  //   //   }
+  //   // };
+  //   let content = this.content.nativeElement;
+  //   doc.fromHTML(content.innerHTML, 15, 15, {
+  //     'width': 300
+  //   });
+  //   doc.save('web.pdf');
+  // }
+
+  // intento con jspdf autotable
   downloadPDF() {
+    let columns = [
+      { title: 'Hora', dataKey: 'hora' },
+      { title: 'Actividad', dataKey: 'nombre'},
+      { title: 'Descripción', dataKey: 'descripcion' }
+    ]
 
-    //   const elementToPrint = document.getElementById('pdf'); //The html element to become a pdf
-    //   const pdf = new jsPDF('auto', 'px', 'a4');
-    //   pdf.addHTML(elementToPrint, () => {
-    //     pdf.save('web.pdf');
-    // });
+    //data o contenido de la tabla
+    let data = [];
+    let rows = { horaInicio: '', horaFin: '', nombre: '' ,descripcion: '' };
 
-    let doc = new jsPDF();
-    let ElementHandlers = {
-      '#editor': function (element, renderer) {
-        return true;
-      }
-    };
-    let content = this.content.nativeElement;
-    doc.fromHTML(content.innerHTML, 15, 15, {
-      'width': 100,
-      'left': 200,
-      'elment': ElementHandlers
+    for (var i = 0; i < this.arrActividades.length; i++) {
+      rows.horaInicio = this.arrActividades[i].horaInicioActividad;
+      rows.horaFin = this.arrActividades[i].horaFinActividad;
+      rows.descripcion = this.arrActividades[i].descripcionActividad;
+      rows.nombre = this.arrActividades[i].nombreActividad;
+
+      data.push({ 'hora': rows.horaInicio + ' - ' + rows.horaFin, 'nombre': rows.nombre, 'descripcion': rows.descripcion });
+      // data.push(rows.horaInicio, rows.horaFin, rows.descripcion);
+    }
+    var doc = new jsPDF({ orientation: 'p', format: 'letter', putOnlyUsedFonts: true });
+    //Configuracion para el nombre del evento
+    doc.setFontSize(22);
+    doc.text(this.evento[0].nombreEvento,14,20);
+
+    //Configuracion para los datos del evento
+    doc.setFontSize(16);
+    doc.text('Fecha: ' + this.fechaEvento,14,30);
+    doc.text('Ubicación: ' + this.evento[0].ubicacion + ', ' + this.evento[0].direccion + ', ' + this.evento[0].ciudad.nombreCiudad, 14, 40 );
+
+    //Configuracion para la tabla con las actividades
+    doc.setFontSize(12);
+    doc.autoTable(columns, data, {
+      columnStyles: {
+        hora: {cellWidth: 27},
+        nombre: {cellWidth: 30},
+        descripcion: {cellWidth: 'auto'}
+      },
+      margin: {top: 50}
     });
-    doc.save('web.pdf');
+
+    //Exportar a pdf
+    doc.save('tabla.pdf');
   }
 }
