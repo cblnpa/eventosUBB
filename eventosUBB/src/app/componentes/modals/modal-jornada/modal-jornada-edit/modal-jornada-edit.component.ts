48b0f000784633b05b9669c98eb5d00007d8d6d4
Camila Pinto <camilapinto@outlook.cl>
2020-01-14 10:08:18 -0300

    Modal Jornada edit, correcciones

--- eventosUBB/src/app/componentes/modals/modal-jornada/modal-jornada-edit/modal-jornada-edit.component.ts
+++ eventosUBB/src/app/componentes/modals/modal-jornada/modal-jornada-edit/modal-jornada-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ModalService, JornadaService } from '../../../../servicios/servicio.index';
 import { jornada } from 'src/app/model/jornada';
 import Swal from 'sweetalert2';
@@ -12,6 +12,7 @@ export class ModalJornadaEditComponent implements OnInit {
 
   public jornadas: jornada;
   public idEvento;
+  public fechaActual;
 
   @Input() idJornadaEdit: number; // recibe el id de la jornada a editar
 
@@ -21,11 +22,25 @@ export class ModalJornadaEditComponent implements OnInit {
 
   ngOnInit() {
     this.getDatosJornada();
+    this.getActualDate();
   }
+  //Obtener fecha actual para bloquear el calendario
+  getActualDate() {
+    var fecha = new Date();
+    var dias = fecha.getDate();
+    var mes = fecha.getMonth() + 1;
+    var year = fecha.getFullYear();
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.jornadas.idJornada = this.idJornadaEdit;
-    this.getDatosJornada();
+    if (mes < 10 && dias < 10)
+      this.fechaActual = year + '-0' + mes + '-0' + dias;
+    else if (mes < 10)
+      this.fechaActual = year + '-0' + mes + '-' + dias;
+    else if (dias < 10)
+      this.fechaActual = year + '-' + mes + '-0' + dias;
+    else
+      this.fechaActual = year + '-' + mes + '-' + dias;
+
+      console.log(this.fechaActual);
   }
 
   getDatosJornada() {
@@ -45,7 +60,7 @@ export class ModalJornadaEditComponent implements OnInit {
   editarJornada(form) {
     this.jornadaService.editJornada(this.jornadas, this.jornadas.idJornada).subscribe(
       response => {
-        if(response){
+        if (response) {
           Swal.fire({
             title: 'Jornada editada',
             type: 'success'
